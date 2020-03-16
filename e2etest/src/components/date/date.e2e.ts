a7214ff7711c82c47a90e24ca9006bc611279cb6
RobinMonro <r.monro1@gmail.com>
2020-03-16 17:59:29 +1100

    Update date.e2e.ts

--- e2etest/src/components/date/date.e2e.ts
+++ e2etest/src/components/date/date.e2e.ts
@@ -1,9 +1,6 @@
 import { newE2EPage, E2EPage, E2EElement } from '@stencil/core/testing';
-import { DateResolution } from '../../../../models/care-record-specs';
-
-import moment, * as _moment from 'moment-timezone';
-import { DateInput } from './date';
-import { TestUtil } from '../../../../models/test-data-crs-generator';
+import { UpdatedValue } from '../date-resources';
+import { DateInput } from 'date';
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -18,11 +15,7 @@ describe('date', () => {
     let page: E2EPage;
     let element: E2EElement;
     beforeEach(async () => {
-      page = await newE2EPage({
-        html: `
-          <sw-date></sw-date>
-        `
-      });
+      page = await newE2EPage({ html: `<sw-date></sw-date>` });
       element = await page.find('sw-date');
     });
 
@@ -32,37 +25,43 @@ describe('date', () => {
     });
 
     it('should work without parameters or data', async () => {
-      // expect(element.textContent.trim()).toEqual('Today//'); // broken
+      expect(element.textContent.trim()).toEqual('//');
     });
 
     describe('validation of inputs', () => {
       let day: E2EElement;
       let dayInput;
-      let dayInput2;
-      let dayInput3;
-      let dayInput4;
       let month;
+      let monthInput;
       let year;
+      let yearInput;
       let validityChangeSpy;
       let updateDateSpy;
       let name;
       beforeEach(async () => {
-        day = await page.find('sw-date >>> .day');
-        dayInput = day.shadowRoot.querySelector('input')
-        dayInput2 = await day.find('date-time-number >>> input');
-        dayInput3 = await day.find('input');
-        dayInput4 = await day.find('date-time-number >>> div');
-        month = await page.find('sw-date >>> .month >>> input');
-        year = await page.find('sw-date >>> .year >>> input');
-        validityChangeSpy = await element.spyOnEvent('validityChange');
+        day = await page.find('test-date >>> .day');
+        dayInput = await page.find('date-time-input >>> input')
+        month = await page.find('sw-date >>> .month');
+        monthInput = await page.find('date-time-input >>> input')
+        year = await page.find('sw-date >>> .year');
+        yearInput = await page.find('date-time-input >>> input')
         updateDateSpy = await element.spyOnEvent('updateDate');
         name = await element.getProperty('name');
       });
 
       describe('day only', () => {
-        it('a good number should be not enough', async () => {
+        it('should respond to inputs', async () => {
+        let value = await element.getProperty('value');
+        expect(value).toBe('0');
+        await page.waitForChanges();
 
+        await dayInput.press('2'); // Oh no! It breaks here because it can't find dayInput
+        await dayInput.press('3');
+        await page.waitForChanges();
 
+        expect(updateValueSpy).toHaveReceivedEventDetail({
+          id: name,
+          value: '23'
         });
       });
     });
